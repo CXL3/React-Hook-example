a57b04b83298811efca3128aae19a453022e9071
Claire Lu <appsfunlu@gmail.com>
2021-11-03 23:23:16 -0700

    useCallback hook example

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 
 import React from 'react';
-import UseStateComponent from './Hooks/UseStateComponent';
+// import UseStateComponent from './Hooks/UseStateComponent';
+import UseCallback from './Hooks/UseCallback';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <UseStateComponent />
+      {/* <UseStateComponent /> */}
+      <UseCallback />
     </View>
   );
 }
